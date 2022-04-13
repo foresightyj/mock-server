1a343b56eac98014ba950e686beeed1be71d770b
Jian Yuan <yuanjian@tesla.com>
2022-04-14 00:17:43 +0800

    minor

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 export interface ApiResponse<T = void> {
     success: boolean,
+    code: number,
     msg?: string,
     data?: T
 }
@@ -7,14 +8,16 @@ export interface ApiResponse<T = void> {
 export function ok<T>(data: T): ApiResponse<T> {
     return {
         success: true,
+        code: 200,
         data,
     }
 }
 
 
-export function fail(error: Error): ApiResponse<any> {
+export function fail(error: Error, code = 400): ApiResponse<any> {
     return {
         success: false,
+        code,
         msg: error?.message ?? "unknown error"
     }
 }
